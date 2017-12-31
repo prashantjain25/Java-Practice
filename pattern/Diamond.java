e1d7550189d968983b30aae536a2e7f45c13850e
prashantjain25 <prashantjain25@gmail.com>
2017-12-31 20:35:49 +0530

    pattern Diamond half done.

--- pattern/Diamond.java
+++ pattern/Diamond.java
@@ -5,19 +5,14 @@ public class Diamond {
 	public static void main(String[] arg) {
 		int n = 6;
 		n = 2 * n - 1;
-
 		int prev = n - (n / 2);
-		for (int k =prev; k <= n; k++) {
-
+		for (int k = prev; k <= n; k++) {
 			for (int j = 1; j < prev; j++)
 				System.out.print(" ");
-
 			callAlt(prev, k);
-
 			prev = prev - 1;
 			System.out.println();
 		}
-
 	}
 
 	public static void callAlt(int prev, int k) {
