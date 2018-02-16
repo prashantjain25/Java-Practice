ecf84a7870453574c47ddc8fe1a4c4b31c989ce4
prashantjain25 <prashantjain25@gmail.com>
2018-02-16 14:57:29 +0530

    New changes

--- binarySearchTree/DFS.java
+++ binarySearchTree/DFS.java
@@ -3,71 +3,70 @@ package binarySearchTree;
 import java.util.LinkedList;
 import java.util.List;
 
-enum visit {
+enum visitDFS {
     yes, no;
 
 };
 
-class Node {
-    String visited=visit.no.toString();
+class NodeDFS {
+    String visited = visitDFS.no.toString();
     int data;
-    List<Node> nodes=new LinkedList<>();
+    List<NodeDFS> nodes = new LinkedList<>();
 }
 
- class Provider{
-     List<Node> nodes=new LinkedList<>();
-     List<Node> getNodes(){
-	 Node e=new Node();
-	 	Node e1=new Node();
-	 	e1.data=7;
-	 		Node e1a=new Node();
-	 		e1a.data=6;
-	 		Node e1b=new Node();
-	 		e1b.data=5;
-	 	e1.nodes.add(e1a);
-	 	e1.nodes.add(e1b);
-	 e.nodes.add(e1);
-	 
-	
-	 	Node i1=new Node();
-	 	i1.data=7;
-	 		Node i1a=new Node();
-	 		i1a.data=6;
-	 		Node i1b=new Node();
-	 		i1b.data=9;
-	 	i1.nodes.add(i1a);
-	 	i1.nodes.add(i1b);
-	 e.nodes.add(i1);
-	 nodes.add(e);
+class Provider {
+    List<NodeDFS> nodes = new LinkedList<>();
+
+    List<NodeDFS> getNodes() {
+	NodeDFS e = new NodeDFS();
+	NodeDFS e1 = new NodeDFS();
+	e1.data = 7;
+	NodeDFS e1a = new NodeDFS();
+	e1a.data = 6;
+	NodeDFS e1b = new NodeDFS();
+	e1b.data = 5;
+	e1.nodes.add(e1a);
+	e1.nodes.add(e1b);
+	e.nodes.add(e1);
+
+	NodeDFS i1 = new NodeDFS();
+	i1.data = 7;
+	NodeDFS i1a = new NodeDFS();
+	i1a.data = 6;
+	NodeDFS i1b = new NodeDFS();
+	i1b.data = 9;
+	i1.nodes.add(i1a);
+	i1.nodes.add(i1b);
+	e.nodes.add(i1);
+	nodes.add(e);
 	return nodes;
     }
 }
+
 public class DFS {
     static boolean found;
+
     public static void main(String[] args) {
-	int N = 5;
-	Node root = new Node();
-	//root.data=10;
+	NodeDFS root = new NodeDFS();
 	root.nodes.addAll(new Provider().getNodes());
-	DFS dfs=new DFS();
-		for(Node n:root.nodes)
-		    {
-		    dfs.search(n,9);
-		    }
-	    System.out.println(found );
-	
+	DFS dfs = new DFS();
+	for (NodeDFS n : root.nodes) {
+	    dfs.search(n, 9);
+	}
+	System.out.println(found);
+
     }
 
-    private void search(Node current, int k) {
-	current.visited = visit.yes.toString();
-	if(current.data==k){
-	    found=true;
-	    return ;
+    private void search(NodeDFS current, int k) {
+	current.visited = visitDFS.yes.toString();
+	if (current.data == k) {
+	    found = true;
+	    return;
 	}
-	    
+
 	if (current != null) {
-	    current.nodes.forEach(x -> search(x,k));
+	    current.nodes.forEach(x -> search(x, k));
 	}
-	    return ;
+	return;
     }
 }
